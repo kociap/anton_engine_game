670dfbcee301236b229fae3fe460b364d4e707f2
Piotr Kocia <p.kocia@wp.pl>
2020-05-18 08:40:42 -0400

    renamed Vector to Array

--- generated/systems.generated.cpp
+++ generated/systems.generated.cpp
@@ -1,4 +1,4 @@
-#include <core/atl/vector.hpp>
+#include <core/atl/array.hpp>
 #include <engine/ecs/system.hpp>
 
 #include <asteroid.hpp>
@@ -6,8 +6,8 @@
 #include <spaceship.hpp>
 
 namespace anton_engine {
-    extern "C" GAME_API atl::Vector<System*> create_systems() {
-        atl::Vector<System*> systems(atl::reserve, 3);
+    extern "C" GAME_API atl::Array<System*> create_systems() {
+        atl::Array<System*> systems(atl::reserve, 3);
         systems.push_back(new asteroids_game::Asteroid_System);
         systems.push_back(new asteroids_game::Spaceship_System);
         systems.push_back(new asteroids_game::Projectile_System);
